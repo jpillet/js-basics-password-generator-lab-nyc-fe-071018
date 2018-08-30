a453b3510819296b6a0585be5013745db11bcf98
jpillet <jpillet@gmail.com>
2018-08-30 22:17:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", () => {
   //start your code here
-});
+let outputResult = document.querySelector("output")
+let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('')
+let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+let numberCharacters = "0123456789".split('')
+let specialCharacters = "!#$%&&()*+,-./:;<=>?@[\\]^_{|}-".split('')
+
+  document.addEventlistetener('submit', (event) => {
+event.preventDefault();
+    
+    let input = document.getElementBy Id("passwordLength")
+  });
